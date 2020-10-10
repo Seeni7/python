be983fbeef2ba8101d790258cffeed3944571b99
seeni7 <Seeni.rengasamy@gmail.com>
2020-10-10 19:17:22 -0400

    python files

---
+++ nested if condition.py
@@ -0,0 +1,16 @@
+str = "hello"
+print(str)
+
+var = 10
+if(var == 10):
+     print("1 var is equvalent to 10")
+     print("good bye")
+
+     if(var < 15):
+         print("var is smaler than 15")
+
+     if(var < 12):
+          print("var is smaller that 12 too")
+     else:
+      print("var is greater than 15")
+
