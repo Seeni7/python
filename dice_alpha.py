99165658ef5e06cb0d9ca67b9cb5c6bfe0394c81
seeni7 <Seeni.rengasamy@gmail.com>
2020-10-11 13:47:11 -0400

    game development

---
+++ dice_alpha.py
@@ -0,0 +1,15 @@
+import random
+
+player = random.randint(1,6)
+
+print("You rolled"+ str(player))
+
+ai = random.randint(1,6)
+
+print("The computer rolled" + str(ai))
+
+""" if player > ai:
+    print("You rolled"+ player)
+else:
+    print("You lose")
+ """
