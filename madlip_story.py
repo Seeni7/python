282c7f5f8b96dbe1d25dd746be59fe475d47e3b0
seeni7 <Seeni.rengasamy@gmail.com>
2020-10-12 18:24:30 -0400

    Madlip Story

---
+++ madlip_story.py
@@ -0,0 +1,14 @@
+noun_1 = input("Name a noun")
+print
+noun_2 = input("Name another noun")
+noun_3 = input("Name yet another noun")
+verb  = input("Give the verb")
+adject = input("Give the adjective")
+animal = input("Give the animal name")
+profession = input("give the profession")
+
+print("%s Dumpty %s on a wall. \
+    %s Dumpty had a/n %s fall.\
+    All the king's %s and the %s's men.\
+    couln't put %s together again."
+    %(noun_1, verb, noun_2, adject, animal, profession, noun_3))
