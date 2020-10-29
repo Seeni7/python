bfb6eae51dd2b55d2746dc5ef9f2556b79f8f5da
seeni7 <Seeni.rengasamy@gmail.com>
2020-10-29 13:16:14 -0400

    python arguments

---
+++ arbit.arg.py
@@ -0,0 +1,8 @@
+def log(sequence, message, *values):
+    if not values:
+        print('%s:%s'% (sequence, message))
+    else:
+        values_str = ','.join(str(x) for x in values)
+        print('%s: %s: %s' %(sequence, message, values_str))
+log(1, 'Favorites', 7, 33)
+log('Favorite', 7, 3)
---
+++ error_hand.py
@@ -0,0 +1,18 @@
+
+file_name = 'test.txt'
+
+try:
+    my_file = open('test.txt', 'x')
+    my_file.write('Meatballs\n')
+    my_file.close()
+except FileExistsError as err:
+    print(f"The {file_name} file already exists.")
+    
+except: 
+    print(f"Unable to write to the file")
+    
+else:
+    print(f"Wrote to {file_name}")
+
+finally:
+    print("Execution completed")
